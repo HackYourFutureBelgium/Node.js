d283168891cd2e7f5b2ed4065ecb001d0e54c038
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-08-30 20:56:48 +0200

    Update server.js

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -2,19 +2,56 @@
 
 const http = require('http');
 
-/* `createServer` MUST return an instance of `http.Server` otherwise the tests
- * will fail.
- */
 function createServer(port) {
   let state = 10;
+  let res = {};
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        console.log('State');
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        res = JSON.stringify({ state: state });
+        response.write(res);
+        response.end();
+        break;
+      case '/add':
+        console.log('Add');
+        state += 1;
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        res = JSON.stringify({ state: state });
+        response.write(res);
+        response.end();
+        break;
+      case '/subtract':
+        console.log('Subtract');
+        state -= 1;
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        res = JSON.stringify({ state: state });
+        response.write(res);
+        response.end();
+        break;
+      case '/reset':
+        console.log('Reset');
+        state = 10;
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        res = JSON.stringify({ state: state });
+        response.write(res);
+        response.end();
+        break;
+      default:
+        console.log('Default');
+        response.writeHead(404, { 'Content-Type': 'application/json' });
+        res = JSON.stringify({ error: 'Not Found' });
+        response.write(res);
+        response.end();
+        break;
+    }
+    console.log(state);
+    return state;
   });
 
   return server;
 }
 
-module.exports = {
-  createServer
-};
+module.exports = createServer;

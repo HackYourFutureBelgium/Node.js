3751132d0d76d1250b31afdcfae8605aec1df43c
Mesut <50662174+Efiloglu@users.noreply.github.com>
2019-08-30 23:55:23 +0200

    updated

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,34 +9,31 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    switch (request.url) {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+      switch (request.url) {
       
       case '/state':
-        response.writeHead(200, { 'Content-Type': 'application/json' });
         response.write(JSON.stringify({ state }));
         break;
   
       case '/add':
         state++;
-        response.writeHead(200, { 'Content-Type': 'application/json' });
         response.write(JSON.stringify({ state }));
         break;
   
       case '/subtract':
         state--;
-        response.writeHead(200, { 'Content-Type': 'application/json' });
         response.write(JSON.stringify({ state }));
         break;
   
       case '/reset':
         state = 10;
-        response.writeHead(200, { 'Content-Type': 'application/json' });
         response.write(JSON.stringify({ state }));
         break;
   
       default:
-          response.writeHead(404, { 'Content-Type': 'application/json' });
-          response.write(JSON.stringify({ error: 'Not found' }));
+        response.writeHead(404, {'Content-Type': 'application/json'});
+        response.write(JSON.stringify({ error: 'Not found' }));
     }
   
     response.end();

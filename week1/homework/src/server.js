4032c7b804e12af969f7a42dfbe4fd7d35d56734
prgrmfl <hrn.acc@protonmail.com>
2019-08-30 14:52:13 +0300

    node.js week1 http server hw refactor

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -5,6 +5,7 @@ const http = require('http');
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
  */
+
 function createServer(port) {
   let state = 10;
 
@@ -20,68 +21,55 @@ function createServer(port) {
        }).listen(8080);
     */
 
-    const url = request.url; //declare url as a variable to use in function
+    const url = request.url; // declare url as a variable to use later
     console.log(url);
 
-    if (url === `/state`) {
+    const res = () => {
       response.writeHead(200, { 'Content-Type': 'application/json' }); // response.writeHead(statusCode[, statusMessage][, headers])
       const resMessage = {
         state: state,
       };
       response.write(JSON.stringify(resMessage)); // transform the response to JSON string and .write() method  sends a chunk of the response body.
-      response.end(); // Always end the response!
+    };
+
+    if (url === `/state`) {
+      res();
     } else if (url === `/add`) {
       state++;
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      const resMessage = {
-        state: state,
-      };
-      response.write(JSON.stringify(resMessage));
-      response.end(); // Always end the response!
+      res();
     } else if (url === `/subtract`) {
       state--;
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      const resMessage = {
-        state: state,
-      };
-      response.write(JSON.stringify(resMessage));
-      response.end(); // Always .end() the response!
+      res();
     } else if (url === `/reset`) {
       state = 10;
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      const resMessage = {
-        state: state,
-      };
-      response.write(JSON.stringify(resMessage));
-      response.end(); // Always .end() the response!
+      res();
     } else {
       response.writeHead(404, { 'Content-Type': 'application/json' });
       const resMessage = {
         error: 'Not found',
       };
       response.write(JSON.stringify(resMessage));
-      response.end(); // Always .end() the response!
     }
+
+    response.end(); // Always .end() the response!
   });
   return server;
 }
 
 /* test result:
-
   √ /state returns 10
   √ /add returns 11
   √ /subtract returns 9
   √ /reset returns 10
   √ querying undefined URL returns 404 Not Found
+  √ /add, /reset returns 10
   √ /subtract, /reset returns 10
-  √ /add, /reset returns 10 (101ms)
-  √ /add, /add, /state, /add, /subtract returns 12 (131ms)
-  √ /subtract, /subtract, /reset, /add, /state, /subtract, /add returns 11 (142ms)
-  √ /add, /add, /add, /add, /add, /add, /add, /add, /add, /add, /state returns 20 (157ms)
-  √ /subtract, /subtract, /subtract, /subtract, /subtract, /subtract, /subtract, /subtract, /subtract, /subtract, /state returns 0 (160ms)
+  √ /add, /add, /state, /add, /subtract returns 12 (102ms)
+  √ /subtract, /subtract, /reset, /add, /state, /subtract, /add returns 11 (126ms)
+  √ /add, /add, /add, /add, /add, /add, /add, /add, /add, /add, /state returns 20 (144ms)
+  √ /subtract, /subtract, /subtract, /subtract, /subtract, /subtract, /subtract, /subtract, /subtract, /subtract, /state returns 0 (145ms)
 
   11 tests passed
-
 */
 
 module.exports = {

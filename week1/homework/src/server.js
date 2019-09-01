15c08dd6c8abe79757e880f7ca94d10e1d4772c6
saliha54 <51030929+saliha54@users.noreply.github.com>
2019-09-01 12:02:02 +0200

    Update server.js

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,7 +9,6 @@ function createServer(port) {
   const server = http.createServer((request, response) => {
     switch (request.url) {
       case '/state':
-<<<<<<< HEAD
         break;
       case '/add':
         state += 1;
@@ -33,46 +32,14 @@ function createServer(port) {
       res = JSON.stringify({ state: state });
     }
     response.end(res);
-=======
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        res = JSON.stringify({ state: state });
-        response.end(res);
-        break;
-      case '/add':
-        state += 1;
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        res = JSON.stringify({ state: state });
-        response.end(res);
-        break;
-      case '/subtract':
-        state -= 1;
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        res = JSON.stringify({ state: state });
-        response.end(res);
-        break;
-      case '/reset':
-        state = 10;
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        res = JSON.stringify({ state: state });
-        response.end(res);
-        break;
-      default:
-        response.writeHead(404, { 'Content-Type': 'application/json' });
-        res = JSON.stringify({ error: 'Not found' });
-        response.end(res);
-        break;
-    }
->>>>>>> 4096553e20400358582830b1be9d0181a0810996
     return state;
   });
 
   return server;
 }
 
-<<<<<<< HEAD
+
 module.exports = {
   createServer,
 };
-=======
-module.exports = { createServer };
->>>>>>> 4096553e20400358582830b1be9d0181a0810996
+

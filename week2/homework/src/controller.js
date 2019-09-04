1a719dc1b596eeab233ee20b3a053b32eac37745
berihugebre <danigebre039@gmail.com>
2019-09-05 01:44:27 +0200

    dirname add to readAndWrite function

--- week2/homework/src/controller.js
+++ week2/homework/src/controller.js
@@ -1,21 +1,22 @@
 'use strict';
 const fs = require('fs');
+const path = require('path');
 
-function readAndWrite(directory, callback) {
-  fs.readFile(directory, 'utf8', (err, data) => {
+function readAndWrite (directory, callback) {
+  fs.readFile(`${(__dirname)}${directory}`, 'utf8', (err, data) => {
     if (err) throw err;
     const parsed = JSON.parse(data);
     const todoListArray = parsed.todo;
     callback(todoListArray);
     const stringed = JSON.stringify(parsed);
-    fs.writeFile(directory, stringed, (err) => {
+    fs.writeFile(`${(__dirname)}${directory}`, stringed, (err) => {
       if (err) throw err;
     });
   });
 }
 
 function toList() {
-  readAndWrite('./store.txt', (todoList) => {
+  readAndWrite(`/store.txt`, (todoList) => {
     if (todoList.length === 0) {
       console.log(`Your list is Empty`);
     }
@@ -26,14 +27,14 @@ function toList() {
 }
 
 function toAdd(cmd) {
-  readAndWrite('./store.txt', (todoList) => {
+  readAndWrite('/store.txt', (todoList) => {
     todoList.push(`${cmd}`);
     console.log(`"${cmd}" is added to the list`);
   });
 }
 
 function toRemove(cmd) {
-  readAndWrite('./store.txt', (todoList) => {
+  readAndWrite('/store.txt', (todoList) => {
     if (cmd > todoList.length) {
       console.log(`the item you are trying to remove is not in the list`);
     }
@@ -45,7 +46,7 @@ function toRemove(cmd) {
 }
 
 function toUpdate(com1, com2) {
-  readAndWrite('./store.txt', (todoList) => {
+  readAndWrite('/store.txt', (todoList) => {
     if (com1 > todoList.length) {
       console.log(`the item you are trying to update is not in the list`);
     }
@@ -57,7 +58,7 @@ function toUpdate(com1, com2) {
 }
 
 function toReset() {
-  readAndWrite('./store.txt', (todoList) => {
+  readAndWrite('/store.txt', (todoList) => {
     todoList.splice(0, todoList.length);
     console.log(`list reseated`);
   });

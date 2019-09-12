5abf277ca815099dc477a6977bba6cbbc1783446
Saliha <salihayaylaci@g.ucla.edu>
2019-09-13 01:18:55 +0200

    week3 howk

--- week3/homework/src/actions/deleteTodo.js
+++ week3/homework/src/actions/deleteTodo.js
@@ -3,14 +3,15 @@
 function deleteTodo(todoManager, request, response) {
   const id = request.params.id;
 
-  todoManager.delete(id)
+  todoManager
+    .delete(id)
     .then(() => {
       response.status(204);
       response.end();
     })
-    .catch(({message}) => {
+    .catch(({ message }) => {
       response.status(500);
-      response.json({error: message});
+      response.json({ error: message });
     });
 }
 
